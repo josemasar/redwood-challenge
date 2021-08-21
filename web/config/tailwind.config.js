13f32f91450e5e8e9910e188934ec267460f9ee2
Josema Sar <josemanuelsarrion@gmail.com>
2021-08-21 18:45:49 +0200

    redwood update v0.36

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
